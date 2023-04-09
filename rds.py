0f58d7f0b3d9642254274546cebc010fde3b0810
Rajeev J Sebastian <rajeev@alokin.in>
2023-04-08 18:25:59 -0700

    adding rds inspection

--- rds.py
+++ rds.py
@@ -2,17 +2,39 @@
 
 import boto3
 import pprint
+import awspricing
 
 client = boto3.client('rds')
 response = client.describe_db_instances()
 
 
+rds_offer = awspricing.offer('AmazonRDS')
 
-for i in response['DBInstances']:
-    pprint.pprint(i)
-    # db_name = i['DBName']
-    db_instance_name = i['DBInstanceIdentifier']
-    db_type = i['DBInstanceClass']
-    db_storage = i['AllocatedStorage']
-    db_engine = i['Engine']
-    print("{0}, {1}, {2}".format(db_instance_name,db_type,db_storage,db_engine))
+sku = rds_offer.search_skus(
+  instance_type='db.m4.large',
+  location='US East (N. Virginia)',
+  database_engine='MySQL',
+  license_model='No license required',
+  deployment_option='Multi-AZ'
+) # {'QPZNR6MYN432XTPU'}
+
+oh = rds_offer.ondemand_hourly(
+  'db.m4.large',
+  'MySQL',
+  license_model='No license required',
+  deployment_option='Multi-AZ',
+  region='us-east-1'
+) # 0.35
+
+print("{0}, {1}".format(sku, oh))
+
+# for i in response['DBInstances']:
+#     pprint.pprint(i)
+#     # db_name = i['DBName']
+#     db_instance_name = i['DBInstanceIdentifier']
+#     db_type = i['DBInstanceClass']
+#     db_storage = i['AllocatedStorage']
+#     db_engine = i['Engine']
+#     print("{0}, {1}, {2}".format(db_instance_name,db_type,db_storage,db_engine))
+#
+
