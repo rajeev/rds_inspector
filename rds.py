a2a5b8943907bc6a5db43eb4ded4f51515a3a352
Rajeev J Sebastian <rajeev@alokin.in>
2023-04-08 17:46:46 -0700

    adding rds inspection

--- rds.py
+++ rds.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import boto3
+import pprint
+
 client = boto3.client('rds')
 response = client.describe_db_instances()
 
+
+
 for i in response['DBInstances']:
+    pprint.pprint(i)
     db_name = i['DBName']
     db_instance_name = i['DBInstanceIdentifier']
     db_type = i['DBInstanceClass']
